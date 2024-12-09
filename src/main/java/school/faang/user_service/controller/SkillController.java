ddeb73028e377d9f25d16f01a73a0ad945ab5f67
axwell-wq <Stepan07mail@gmail.com>
2024-12-09 18:17:10 +0300

    не доделал два метода, нужна следующая задача

---
+++ src/main/java/school/faang/user_service/controller/SkillController.java
@@ -0,0 +1,22 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.service.SkillService;
+import school.faang.user_service.skill_dto.SkillDto;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class SkillController {
+    private final SkillService skillService;
+
+    public SkillDto create(SkillDto skillDto) {
+        return skillService.create(skillDto);
+    }
+
+    public List<SkillDto> getUserSkills(Long userId) {
+        return skillService.getUserSkills(userId);
+    }
+}
