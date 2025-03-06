c30610780d579ba17d4b88840d519e18d35acd0d
axwell-wq <Stepan07mail@gmail.com>
2025-03-06 22:59:30 +0300

    Добавил Rest, но при запуске приложения, выходит ошибка

--- src/main/java/school/faang/user_service/controller/EventController.java
+++ src/main/java/school/faang/user_service/controller/EventController.java
@@ -2,38 +2,46 @@ package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.EventDto;
 import school.faang.user_service.service.EventService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/event")
 public class EventController {
 
     private final EventService eventService;
 
-    public EventDto create(EventDto event) {
+    @PostMapping("/create")
+    public EventDto create(@RequestBody EventDto event) {
         return eventService.create(event);
     }
 
-    public EventDto getEvent(Long eventId) {
+    @GetMapping("/getevent{eventId}")
+    public EventDto getEvent(@PathVariable Long eventId) {
         return eventService.getEvent(eventId);
     }
 
-    public void deleteEvent(Long eventId) {
+    @DeleteMapping("/delete{eventId}")
+    public void deleteEvent(@PathVariable Long eventId) {
         eventService.deleteEvent(eventId);
     }
 
-    public EventDto updateEvent(EventDto event) {
+    @PutMapping("/update")
+    public EventDto updateEvent(@RequestBody EventDto event) {
         return eventService.updateEvent(event);
     }
 
-    public List<EventDto> getOwnedEvents(Long userId) {
+    @GetMapping("/getownedevents{userId}")
+    public List<EventDto> getOwnedEvents(@PathVariable Long userId) {
         return eventService.getOwnedEvents(userId);
     }
 
-    public List<EventDto> getParticipatedEvents(Long userId) {
+    @GetMapping("/getparticipatedevents{userId}")
+    public List<EventDto> getParticipatedEvents(@PathVariable Long userId) {
         return eventService.getParticipatedEvents(userId);
     }
 }
